2558a4f68cf9989ba4178afc016803e3fbc48a27
danny vincent <87720409+1oupd@users.noreply.github.com>
2021-10-26 16:57:44 +0100

    clean-up

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div>
+      <h1>Can't pick a mood for today?</h1>
+      <h2>how about this:</h2>
+    </div>
+  );
+}
+
+export default Header;
